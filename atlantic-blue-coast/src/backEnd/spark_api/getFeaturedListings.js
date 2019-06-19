82060e44734ab75c3501815ad2d7f726c0d1fe3b
Billy <Billy.Erwin@ncino.com>
2019-06-19 18:26:53 -0400

    wtf

---
+++ atlantic-blue-coast/src/backEnd/spark_api/getFeaturedListings.js
@@ -0,0 +1,39 @@
+let request = require('request');
+
+let token = 'alecxrelpen098otxjtg31p2r';
+let agentId = '20110315124649945876000000';
+
+let listingFilter = encodeURI(`_filter=ListAgentId Eq '${agentId}' And BathsFull Eq 3 And BedsTotal Eq 4`);
+
+let options = {
+	url: `https://sparkapi.com/v1/listings?_expand=PrimaryPhoto&${listingFilter}&_limit=4`,
+	method: 'GET',
+	headers: {
+		'X-SparkApi-User-Agent': 'SparkAPIExamples',
+		'Authorization': `Bearer ${token}`,
+		'Accept': 'application/json',
+		'Content-Type': 'application/json'
+	}
+};
+
+var incomingResponse;
+
+function callback(error, response, body) {
+	if (!error && response.statusCode == 200) {
+		let info = JSON.parse(body);
+		incomingResponse.writeHead(200, {
+			'Content-Type': 'application/json',
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Headers': 'X-Requested-With'
+		});
+		incomingResponse.end(JSON.stringify(info.D.Results));
+	} else {
+		console.log('error : ', error);
+		console.log('response.statusCode : ', response.statusCode)
+	}
+}
+
+module.exports.getFeaturedListings = function(resp){
+	incomingResponse = resp;
+	request(options, callback);
+}
---
+++ atlantic-blue-coast/src/backEnd/spark_api/getSelectedListing.js
@@ -0,0 +1,36 @@
+let request = require('request');
+
+let token = 'alecxrelpen098otxjtg31p2r';
+
+let options = {
+	method: 'GET',
+	headers: {
+		'X-SparkApi-User-Agent': 'SparkAPIExamples',
+		'Authorization': `Bearer ${token}`,
+		'Accept': 'application/json',
+		'Content-Type': 'application/json'
+	}
+};
+
+var incomingResponse;
+
+function callback(error, response, body) {
+	if (!error && response.statusCode == 200) {
+		let info = JSON.parse(body);
+		incomingResponse.writeHead(200, {
+			'Content-Type': 'application/json',
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Headers': 'X-Requested-With'
+		});
+		incomingResponse.end(JSON.stringify(info.D.Results));
+	} else {
+		console.log('error : ', error);
+		console.log('response.statusCode : ', response.statusCode)
+	}
+}
+
+module.exports.getSelectedListing = function(resp, id){
+	options.url = `https://sparkapi.com/v1/listings/${id}?_expand=Photos`;
+	incomingResponse = resp;
+	request(options, callback);
+}
