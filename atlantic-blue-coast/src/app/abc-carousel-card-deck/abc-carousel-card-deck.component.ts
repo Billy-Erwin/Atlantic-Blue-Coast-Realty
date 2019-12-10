5e5ea72546f1461a68e65bb94e9fabd4201bb0d6
Billy <Billy.Erwin@ncino.com>
2019-12-10 00:02:13 -0500

    - almost there

--- atlantic-blue-coast/src/app/abc-carousel-card-deck/abc-carousel-card-deck.component.ts
+++ atlantic-blue-coast/src/app/abc-carousel-card-deck/abc-carousel-card-deck.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ListingsService } from "../listings.service";
 import { Router } from "@angular/router";
 
@@ -11,75 +11,57 @@ export class AbcCarouselCardDeckComponent implements OnInit{
 
 	@Input() listings;
 	title: string = 'Atlantic Blue Coast Featured Listings';
-	numberOfSlides: number = 3;
 	aClassILike;
 	constructor(private router: Router, private listingsService: ListingsService) { }
 
 	ngOnInit() {
-		let windowWidth = $(window).width();
-		console.log('get width it : ', windowWidth);
+		this.updateTheSlides($(window).width());
+
+		$(window).resize(() => {
+			this.updateTheSlides($(window).width());
+			this.setCarouselSize();
+		});
+
+	}
+
+	updateTheSlides(windowWidth: number) {
+
+		let aNewClass = '';
+		let theOldClass = this.aClassILike;
+
 		if(windowWidth > 1199){
-			this.aClassILike = 'col-3';
+			aNewClass = 'col-3';
 		} else if (windowWidth >= 992 && windowWidth < 1200) {
-			this.aClassILike = 'col-4';
+			aNewClass = 'col-4';
 		} else if (windowWidth >= 768 && windowWidth < 992) {
-			this.aClassILike = 'col-6';
+			aNewClass = 'col-6';
 		} else if (windowWidth  < 768) {
-			this.aClassILike = 'col-12';
+			aNewClass = 'col-12';
 		}
+		this.aClassILike = aNewClass;
+
+		$('#recipeCarousel .carousel-item .thisOne').each(function(){
+			$(this).removeClass(theOldClass);
+			console.log($(this));
+			$(this).addClass(aNewClass);
+		});
 	}
 
 	ngAfterViewInit() {
 		let theRouter = this.router;
-		let windowWidth = $(window).width();
-		console.log('get width it : ', windowWidth);
-		// if(windowWidth > 1199){
-		// 	this.aClassILike = 'col-3';
-		// } else if (windowWidth >= 992 && windowWidth < 1200) {
-		// 	this.aClassILike = 'col-4';
-		// } else if (windowWidth >= 768 && windowWidth < 992) {
-		// 	this.aClassILike = 'col-6';
-		// } else if (windowWidth  < 768) {
-		// 	this.aClassILike = 'col-12';
-		// }
-		this.setCarouselSize(this.numberOfSlides);
+		this.setCarouselSize();
+		// this.setCarouselSize(this.numberOfSlides);
 
 		$('.listing-card').each(function(){
 			$(this).click(function () {
 				theRouter.navigate(['/selected-listing', this.id])
 			});
 		});
-
-		$(window).resize(() => {
-			// console.log('this.width : ', $(window).width());
-			let startedState = this.numberOfSlides;
-			let windowWidth = $(window).width();
-			if(windowWidth > 1199){
-				startedState = 4;
-				this.aClassILike = 'col-3';
-			} else if (windowWidth >= 992 && windowWidth < 1200) {
-				startedState = 3;
-				this.aClassILike = 'col-4';
-			} else if (windowWidth >= 768 && windowWidth < 992) {
-				startedState = 2;
-				this.aClassILike = 'col-6';
-			} else if (windowWidth  < 768) {
-				startedState = 1;
-				this.aClassILike = 'col-12';
-			}
-			// if(startedState != this.numberOfSlides){
-			// 	this.setCarouselSize(startedState);
-			// }
-		})
 	}
 
-	setCarouselSize(theNumber: number){
-		let theRouter = this.router;
-		let itStartsWith = 0;
+	setCarouselSize(){
 		$('#recipeCarousel .carousel-item').each(function(){
-			console.log('it starts with : ', itStartsWith);
-			itStartsWith++;
-			let minPerSlide = theNumber;
+			let minPerSlide = 4;
 			let next = $(this).next();
 			if (!next.length) {
 				next = $(this).siblings(':first');
@@ -87,7 +69,6 @@ export class AbcCarouselCardDeckComponent implements OnInit{
 			next.children(':first-child').clone().appendTo($(this));
 
 			for (let i = 0; i < minPerSlide; i++){
-				console.log('index : ' , i);
 				next = next.next();
 				if(!next.length){
 					next = $(this).siblings(':first');
