94339a40e2e79457493a875cec33dd5f9240a041
Billy <Billy.Erwin@ncino.com>
2019-12-17 23:20:19 -0500

    - Finish front end for sort - do some other formating on centering listing card deck

--- atlantic-blue-coast/src/app/abc-sort-listings/abc-sort-listings.component.ts
+++ atlantic-blue-coast/src/app/abc-sort-listings/abc-sort-listings.component.ts
@@ -13,13 +13,13 @@ export class AbcSortListingsComponent implements OnInit {
 	constructor() { }
 
 	ngOnInit() {
-		this.sortIndicator = 'Sort';
 		this.sortOptions = [
-			{sortKey: '+ListPrice', displayName: 'Price Low to High'},
 			{sortKey: '-ListPrice', displayName: 'Price High to Low'},
-			{sortKey: '+BuildingAreaTotal', displayName: 'Sqft Low to High'},
-			{sortKey: '-BuildingAreaTotal', displayName: 'Sqft High to Low'}
+			{sortKey: '+ListPrice', displayName: 'Price Low to High'},
+			{sortKey: '-BuildingAreaTotal', displayName: 'Sqft High to Low'},
+			{sortKey: '+BuildingAreaTotal', displayName: 'Sqft Low to High'}
 		];
+		this.sortIndicator = this.sortOptions[0].displayName;
 	}
 
 	changeSort(sortIndicator: number){
